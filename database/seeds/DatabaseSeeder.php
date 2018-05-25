4d2681d729bf556dce23e6ac0f4a852e36b02e74
prasanth s <prasanths@prasanths-MacBook-Air.local>
2018-05-26 02:09:22 +0530

    Factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(User::class)->create(
+            [
+                'email' => 'prasanthats@gmail.com',
+                'name'=> 'prasanth'
+            ]
+        );
+        factory(User::class)->create(
+            [
+                'email' => 'santh@gmail.com',
+                'name'=> 'santh'
+            ]
+        );
+        factory(User::class)->create(
+            [
+                'email' => 'jaiprasanth@gmail.com',
+                'name'=> 'jaiprasanth'
+            ]
+        );
     }
 }
